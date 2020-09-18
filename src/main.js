58825edab37ae0819ab05346fc4a9dc8482a94ea
Peter Rogers <peter.rogers@gmai.com>
2020-09-18 15:50:33 -0400

    Add click handler to make matches lightable

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 
 let matches = [];
 
-function handleClickMatch(match) {
-    console.log('clicked', match);
+function createMatchElement()
+{
+    let div = document.createElement('div');
+    div.classList.add('match');
+
+    let matchImg = document.createElement('img');
+    matchImg.classList.add('unlit-match');
+    matchImg.src = 'match.svg';
+
+    let flameImg = document.createElement('img');
+    flameImg.classList.add('flame');
+    flameImg.src = 'flame.svg';
+
+    div.appendChild(matchImg);
+    div.appendChild(flameImg);
+
+    return div;
+}
+
+function handleClickMatch(match)
+{
+    match.element.classList.add('lit');
 }
 
 function loaded()
@@ -17,7 +37,7 @@ function loaded()
         matches.push({
             x: Math.random()*0.9 + 0.05,
             y: Math.random()*0.85 + 0.05,
-            element: document.createElement('img'),
+            element: createMatchElement(),
         });
 
         (function(match) {
@@ -37,7 +57,6 @@ function resize()
     for (let n = 0; n < matches.length; n++)
     {
         let match = matches[n];
-        match.element.src = 'match.svg';
         match.element.style.left = match.x*rect.width + 'px';
         match.element.style.top = match.y*rect.height + 'px';
 
